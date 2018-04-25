b76a3fcd1fcaa2f5a64139bec32d1c8123bd01c1
vashelektriktut <38526565+vashelektriktut@users.noreply.github.com>
2018-04-25 21:45:14 +0300

    Add files via upload

---
+++ assets/js/mailto.js
@@ -0,0 +1,34 @@
+﻿$('#contact-form').on('submit', function (event) {
+    event.preventDefault(); // предотвратим отправку формы - действие по умолчанию
+    var that = $(event.target); // получаем ссылку на источник события - форму #contact-form            
+    $.ajax({ // отправляем данные          
+        // URL развернутого скрипта Google Apps Script
+        url: 'https://script.google.com/macros/s/AKfycbw1SkiGMqlNW3Xu2nXcgAN7eDD55jJE6xFfpvGOlHgnZ3Jpi0Q/exec',
+        data: $(this).serialize(), // собираем запрос
+        jsonp: 'cb', // имя параметра запроса
+        jsonpCallback: 'bingo', // имя функции
+        dataType: 'jsonp', // тип данных
+        success: function bingo(data) {
+            console.log(data); // проверим данные, полученные с бэкэнда
+            if (data == 'Error') {
+                alertForm({ form: that, type: 'alert-danger', msg: 'Не удалось отправить сообщение' });
+                return;
+            }
+            alertForm({ form: that, type: 'alert-success', msg: 'Ваше сообщение отправлено' });
+            that.find('textarea').val('');
+        },
+        error: function () {
+            alertForm({ form: that, type: 'alert-danger', msg: 'Не удалось отправить сообщение' });
+        }
+    });
+});
+
+// функция вывода сообщений в модальную форму
+function alertForm(alert) {
+    var div = $('<div class="alert ' + alert.type + '" style="display: none;">' + alert.msg + '</div>');
+    alert.form.prepend(div);
+    div.slideDown(400).delay(3000).slideUp(400, function () {
+        alert.form.closest('.modal').modal('hide');
+        div.remove();
+    });
+}
